b603538323782d6b0f022a5ade5805d27c32f794
Cody Nolden <cody.nolden@usaa.com>
2020-09-28 16:49:13 +0000

    add stg_payments model

--- models/customers.sql
+++ models/customers.sql
@@ -15,18 +15,22 @@ orders as (
 
 ),
 
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
 
 customer_orders as (
 
     select
-        customer_id,
+        o.customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as most_recent_order_date,
+        count(o.order_id) as number_of_orders,
+        sum(case when p.status = 'success' then p.amount end) as lifetime_value
 
-    from orders
-
+    from orders o
+    left join payments p on (p.order_id = o.order_id)
     group by 1
 
 ),
@@ -40,7 +44,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from customers
 
---
+++ models/stg_payments.sql
@@ -0,0 +1,7 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100.0 as amount
+from raw.stripe.payment
